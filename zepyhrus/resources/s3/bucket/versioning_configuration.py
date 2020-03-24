abc5bb965fb07b6c932c9521208c0f4cc54b7c9c
Mutlu Polatcan <mutlupolatcan@gmail.com>
2020-03-24 18:19:28 +0300

    allowed_values validation fix for s3

--- zepyhrus/resources/s3/bucket/versioning_configuration.py
+++ zepyhrus/resources/s3/bucket/versioning_configuration.py
@@ -3,6 +3,6 @@ from .constants import AllowedValues
 
 
 class VersioningConfiguration(Model):
-    @Validator.validate(type=str, status=AllowedValues.VERSIONING_CONFIGURATION_STATUS)
+    @Validator.validate(type=str, allowed_values=AllowedValues.VERSIONING_CONFIGURATION_STATUS)
     def Status(self, value: str):
         return self._set_field(self.Status.__name__, value)
